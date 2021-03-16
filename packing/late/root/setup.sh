93494199bb72365f72c8fa9318a404fb78a2cb2e
James Vodanovich <james@jvodan.com>
2021-03-17 08:38:57 +1100

    manually add getenvoy repo

--- packing/late/root/setup.sh
+++ packing/late/root/setup.sh
@@ -3,8 +3,9 @@ curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
 
 
 
- sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
+echp "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb buster stable stable" > /etc/apt/sources.list.d/envoy.list
 
- sudo apt update
 
- sudo apt install getenvoy-envoy
+  apt-get update
+
+  apt-get install getenvoy-envoy
